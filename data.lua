326127e4e318be836383fa5925cab18270050f22
Freaky_Lord <64467666+LordFreaky@users.noreply.github.com>
2026-06-29 21:11:07 +0200

    Update data.lua

--- data.lua
+++ data.lua
@@ -1,26 +1,21 @@
-local signal_prerequisite = "rocket-silo"
-
 data:extend({
   {
     type = "technology",
     name = "heliopause-foundry-signal-from-space",
     icon = "__base__/graphics/technology/rocket-silo.png",
     icon_size = 256,
+
     enabled = false,
-    visible_when_disabled = false,
-    prerequisites = {signal_prerequisite},
-    effects = {},
-    unit = {
-      count = 500,
-      ingredients = {
-        {"automation-science-pack", 1},
-        {"logistic-science-pack", 1},
-        {"chemical-science-pack", 1},
-        {"production-science-pack", 1},
-        {"utility-science-pack", 1}
-      },
-      time = 30
+    visible_when_disabled = true,
+
+    prerequisites = {"rocket-silo"},
+
+    research_trigger = {
+      type = "scripted"
     },
+
+    effects = {},
+
     order = "z-h-f-a"
   }
 })
