7ab09dc621cfab6854f5e60a9b223790a6e87ebc
Freaky_Lord <64467666+LordFreaky@users.noreply.github.com>
2026-06-30 17:26:53 +0200

    Update data.lua

--- data.lua
+++ data.lua
@@ -1,9 +1,9 @@
 local signal_tech = "heliopause-foundry-signal-from-space"
 local foundry_base = "heliopause-foundry-base"
 local foundry_discovery = "heliopause-foundry-discover-foundry-base"
-
 local solar_system_edge_tech = "stellar-discovery-solar-system-edge"
 
+local signal_icon = "__heliopause-foundry__/graphics/technology/space-signal.png"
 local foundry_base_icon = "__heliopause-foundry__/graphics/space-locations/foundry-base.png"
 local foundry_base_icon_size = 1254
 
@@ -26,7 +26,7 @@ data:extend({
   {
     type = "technology",
     name = signal_tech,
-    icon = "__heliopause-foundry__/graphics/technology/space-signal.png",
+    icon = signal_icon,
     icon_size = 1024,
 
     enabled = false,
@@ -93,8 +93,20 @@ data:extend({
   {
     type = "technology",
     name = foundry_discovery,
-    icon = foundry_base_icon,
-    icon_size = foundry_base_icon_size,
+
+    icons = {
+      {
+        icon = foundry_base_icon,
+        icon_size = foundry_base_icon_size
+      },
+      {
+        icon = "__space-age__/graphics/icons/planet-route.png",
+        icon_size = 64,
+        scale = 0.65,
+        shift = {-72, 72},
+        draw_background = true
+      }
+    },
 
     prerequisites = {
       "planet-discovery-aquilo"
@@ -131,113 +143,4 @@ data:extend({
   }
 })
 
-add_prerequisite(solar_system_edge_tech, foundry_discovery)local signal_tech = "heliopause-foundry-signal-from-space"
-local foundry_base = "heliopause-foundry-base"
-local foundry_discovery = "heliopause-foundry-discover-foundry-base"
-
-local foundry_base_icon = "__heliopause-foundry__/graphics/space-locations/foundry-base.png"
-local foundry_base_icon_size = 1254
-
-data:extend({
-  {
-    type = "technology",
-    name = signal_tech,
-    icon = "__heliopause-foundry__/graphics/technology/space-signal.png",
-    icon_size = 1024,
-
-    enabled = false,
-    visible_when_disabled = true,
-
-    prerequisites = {"radar"},
-
-    research_trigger = {
-      type = "scripted"
-    },
-
-    effects = {},
-
-    order = "z-h-f-a"
-  }
-})
-
-local solar_system_edge = data.raw["space-location"]["solar-system-edge"]
-local foundry_planet = table.deepcopy(data.raw.planet["nauvis"])
-
-foundry_planet.name = foundry_base
-foundry_planet.localised_name = {"space-location-name.heliopause-foundry-base"}
-foundry_planet.localised_description = {"space-location-description.heliopause-foundry-base"}
-foundry_planet.icon = foundry_base_icon
-foundry_planet.icon_size = foundry_base_icon_size
-
-foundry_planet.distance = solar_system_edge.distance + 2
-foundry_planet.orientation = solar_system_edge.orientation + 0.015
-foundry_planet.magnitude = 1.1
-foundry_planet.map_seed_offset = 424242
-foundry_planet.label_orientation = 0.15
-foundry_planet.parked_platforms_orientation = 0.25
-
-data:extend({
-  foundry_planet,
-
-  {
-    type = "space-connection",
-    name = "solar-system-edge-to-heliopause-foundry-base",
-    from = "solar-system-edge",
-    to = foundry_base,
-    length = 2500,
-    icon = foundry_base_icon,
-    icon_size = foundry_base_icon_size
-  },
-
-  {
-  type = "technology",
-  name = foundry_discovery,
-
-  icons = {
-    {
-      icon = foundry_base_icon,
-      icon_size = foundry_base_icon_size
-    },
-    {
-      icon = "__space-age__/graphics/icons/planet-route.png",
-      icon_size = 64,
-      scale = 0.65,
-      shift = {-72, 72},
-      draw_background = true
-    }
-  },
-
-  prerequisites = {
-    "planet-discovery-aquilo"
-  },
-
-    effects = {
-      {
-        type = "unlock-space-location",
-        space_location = foundry_base,
-        icon = foundry_base_icon,
-        icon_size = foundry_base_icon_size
-        use_icon_overlay_constant = true
-      }
-    },
-
-    unit = {
-      count = 2000,
-      ingredients = {
-        {"automation-science-pack", 1},
-        {"logistic-science-pack", 1},
-        {"chemical-science-pack", 1},
-        {"production-science-pack", 1},
-        {"utility-science-pack", 1},
-        {"space-science-pack", 1},
-        {"metallurgic-science-pack", 1},
-        {"electromagnetic-science-pack", 1},
-        {"agricultural-science-pack", 1},
-        {"cryogenic-science-pack", 1}
-      },
-      time = 60
-    },
-
-    order = "z-h-f-b"
-  }
-})
+add_prerequisite(solar_system_edge_tech, foundry_discovery)
