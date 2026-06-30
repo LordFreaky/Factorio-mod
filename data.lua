5971c6395f5089083875983585225b7846cd8541
Freaky_Lord <64467666+LordFreaky@users.noreply.github.com>
2026-06-30 17:05:35 +0200

    Update data.lua

--- data.lua
+++ data.lua
@@ -1,7 +1,14 @@
+local signal_tech = "heliopause-foundry-signal-from-space"
+local foundry_base = "heliopause-foundry-base"
+local foundry_discovery = "heliopause-foundry-discover-foundry-base"
+
+local foundry_base_icon = "__heliopause-foundry__/graphics/space-locations/foundry-base.png"
+local foundry_base_icon_size = 1254
+
 data:extend({
   {
     type = "technology",
-    name = "heliopause-foundry-signal-from-space",
+    name = signal_tech,
     icon = "__heliopause-foundry__/graphics/technology/space-signal.png",
     icon_size = 1024,
 
@@ -20,17 +27,14 @@ data:extend({
   }
 })
 
-local foundry_base = "heliopause-foundry-base"
-local foundry_discovery = "heliopause-foundry-discover-foundry-base"
-
 local solar_system_edge = data.raw["space-location"]["solar-system-edge"]
 local foundry_planet = table.deepcopy(data.raw.planet["nauvis"])
 
 foundry_planet.name = foundry_base
 foundry_planet.localised_name = {"space-location-name.heliopause-foundry-base"}
 foundry_planet.localised_description = {"space-location-description.heliopause-foundry-base"}
-foundry_planet.icon = "__heliopause-foundry__/graphics/space-locations/foundry-base.png"
-foundry_planet.icon_size = 1024
+foundry_planet.icon = foundry_base_icon
+foundry_planet.icon_size = foundry_base_icon_size
 
 foundry_planet.distance = solar_system_edge.distance + 2
 foundry_planet.orientation = solar_system_edge.orientation + 0.015
@@ -48,28 +52,28 @@ data:extend({
     from = "solar-system-edge",
     to = foundry_base,
     length = 2500,
-    icon = "__base__/graphics/icons/radar.png",
-    icon_size = 1254
+    icon = foundry_base_icon,
+    icon_size = foundry_base_icon_size
   },
 
   {
     type = "technology",
     name = foundry_discovery,
-    icon = "__heliopause-foundry__/graphics/space-locations/foundry-base.png",
-    icon_size = 1254,
+    icon = foundry_base_icon,
+    icon_size = foundry_base_icon_size,
 
     prerequisites = {
       "stellar-discovery-solar-system-edge"
     },
 
     effects = {
-  {
-    type = "unlock-space-location",
-    space_location = foundry_base,
-    icon = "__heliopause-foundry__/graphics/space-locations/foundry-base.png",
-    icon_size = 1254
-  }
-},
+      {
+        type = "unlock-space-location",
+        space_location = foundry_base,
+        icon = foundry_base_icon,
+        icon_size = foundry_base_icon_size
+      }
+    },
 
     unit = {
       count = 2000,
@@ -87,10 +91,6 @@ data:extend({
       },
       time = 60
     },
-
-    order = "z-h-f-b"
-  }
-})
 
     order = "z-h-f-b"
   }
